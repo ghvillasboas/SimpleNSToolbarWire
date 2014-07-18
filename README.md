c499a0f1296a9db6ba2e6bd4a1cd29c453a40a61
George Villasboas <george@logics.com.br>
2014-07-18 10:32:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple NSToolbar Wire (in Swift)
+=================
+
+Code that demonstrates how to wire up an NSToolbar/NSToolbarItem in Swift and NSStoryboards.
+
+Question answered at Stackoverflow. More info at: http://stackoverflow.com/questions/24219441/how-to-use-nstoolbar-in-xcode-6-and-storyboard/24810534?noredirect=1#comment38523066_24810534
--- SimpleToolbarWire/ViewController.swift
+++ SimpleToolbarWire/ViewController.swift
@@ -8,12 +8,17 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSWindowDelegate {
                             
+
+    @IBOutlet var clickLabel: NSTextField
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var window = NSApplication.sharedApplication().windows[0] as NSWindow
+        window.delegate = self;
                                     
     }
 
@@ -24,6 +29,10 @@ class ViewController: NSViewController {
                                     
     }
 
+    @IBAction func clickMe(sender: AnyObject) {
+        println("Toolbar item clicked")
+        clickLabel.stringValue = "Toolbar Working now!"
+    }
 
 }
 
