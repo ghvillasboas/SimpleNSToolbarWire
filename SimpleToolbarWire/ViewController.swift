b0e6708ef2434495026b677f7525ed2e5b70a540
George Villasboas <george@logics.com.br>
2014-07-18 09:24:17 -0300

    Initial Commit

---
+++ SimpleToolbarWire/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SimpleToolbarWire
+//
+//  Created by George Villasboas on 7/18/14.
+//  Copyright (c) 2014 CocoaHeads Brasil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+                                    
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+                                    
+    }
+
+
+}
+
---
+++ SimpleToolbarWire/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SimpleToolbarWire
+//
+//  Created by George Villasboas on 7/18/14.
+//  Copyright (c) 2014 CocoaHeads Brasil. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
